120418b2062e8004fd2858f1509881e20d0612fe
Avik Dutta <googolrj@gmail.com>
2020-07-11 00:25:28 +0530

    clear some form element after registration

--- src/main/resources/static/js/utility.js
+++ src/main/resources/static/js/utility.js
@@ -96,8 +96,7 @@ $(document).ready(function() {
             board: $("#mpboard").val(),
             year: $("#mpyear").val(),
             total: $("#mptotal").val(),
-            marks: $("#mpmarks").val(),
-            percentage: $("#mppercent").val()
+            marks: $("#mpmarks").val()
         };
         academics[0] = mp;
         var hs = {
@@ -105,8 +104,7 @@ $(document).ready(function() {
             board: $("#hsboard").val(),
             year: $("#hsyear").val(),
             total: $("#hstotal").val(),
-            marks: $("#hsmarks").val(),
-            percentage: $("#hspercent").val()
+            marks: $("#hsmarks").val()
         };
         academics[1] = hs;
         var gr = {
@@ -114,8 +112,7 @@ $(document).ready(function() {
             board: $("#gradboard").val(),
             year: $("#gradyear").val(),
             total: $("#gradtotal").val(),
-            marks: $("#gradmarks").val(),
-            percentage: $("#gradpercent").val()
+            marks: $("#gradmarks").val()
         };
         academics[2] = gr;
         var pg = {
@@ -123,8 +120,7 @@ $(document).ready(function() {
             board: $("#pgboard").val(),
             year: $("#pgyear").val(),
             total: $("#pgtotal").val(),
-            marks: $("#pgmarks").val(),
-            percentage: $("#pgpercent").val()
+            marks: $("#pgmarks").val()
         };
         academics[3] = pg;
         var student = {
@@ -166,6 +162,7 @@ $(document).ready(function() {
                 $('html,body').animate({
                     scrollTop: $("#msg").offset().top
                 }, 'slow');
+                $('.clearit').val('');
                 setTimeout(function() {
                     $("#msg").hide();
                 }, 3000);
