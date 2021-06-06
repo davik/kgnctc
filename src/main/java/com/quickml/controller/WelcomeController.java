8ac1634a082d57c5aefa9a22756ef96f76d2ac9f
davik <googolrj@gmail.com>
2021-06-06 18:11:34 +0530

    fixed Due amount calculation in CreatePayment

--- src/main/java/com/quickml/controller/WelcomeController.java
+++ src/main/java/com/quickml/controller/WelcomeController.java
@@ -393,7 +393,7 @@ public class WelcomeController {
 					}
 					paid += pt.amount;
 				}
-				due = savedEntity.courseFee - paid;
+				due = savedEntity.courseFee - GetPaid(savedEntity);
 
 	    		PaymentBody pb = new PaymentBody();
 	    		pb.mobiles = "91" + savedEntity.mobile;
